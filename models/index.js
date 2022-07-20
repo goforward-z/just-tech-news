cb8456426e15e752fe4a03939dfe288a4e1475d7
gofoward-z <zlepeng.ucf@gmail.com>
2022-07-20 17:08:19 -0400

    count votes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // import all models
 const Post = require('./Post');
 const User = require('./User');
+const Vote = require('./vote');
 
 // create associations
 User.hasMany(Post, {
@@ -11,4 +12,33 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'vote_posts',
+  foreignKey: 'user_id'
+});
+
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'vote_posts',
+  foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };

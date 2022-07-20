13f974ce56461694fbf7763d9bb5da3b3ba6d040
gofoward-z <zlepeng.ucf@gmail.com>
2022-07-20 00:08:52 -0400

    add post routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -18,7 +18,13 @@ router.get('/:id', (req, res) => {
     attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
-    }
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ['id', 'title', 'post_url', 'created_at']
+      }
+    ]
   })
     .then(dbUserData => {
       if (!dbUserData) {
@@ -48,19 +54,20 @@ router.post('/', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  //expects {email:"", password:''}
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email
     }
   }).then(dbUserData => {
-    if(!dbUserData) {
-      res.status(400).json({message:'No user with that email addresss'});
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
       return;
     }
-    
+
     const validPassword = dbUserData.checkPassword(req.body.password);
-    if(!validPassword) {
+
+    if (!validPassword) {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
@@ -74,7 +81,7 @@ router.put('/:id', (req, res) => {
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
-    individualHooks:true,
+    individualHooks: true,
     where: {
       id: req.params.id
     }

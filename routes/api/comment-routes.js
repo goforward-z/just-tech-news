d92f83961a78b44e0dae2b7148c30f33613a6da9
gofoward-z <zlepeng.ucf@gmail.com>
2022-07-23 08:41:18 -0400

    add ablitily comment

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.get('/', (req,res) => {});
+router.post('/', (req,res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbcommentData => res.json(dbcommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
+router.delete('/:id',(req,res) => {});
+
+module.exports = router;
